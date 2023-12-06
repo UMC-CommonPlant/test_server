0ecd24c9cdd4e626d52ba8843e8b13fbe8673c7c
음휘영 <76817418+hweyoung@users.noreply.github.com>
2023-12-06 17:45:07 +0900

    Update CommonplantApplicationTests.java

--- src/test/java/com/umc/commonplant/CommonplantApplicationTests.java
+++ src/test/java/com/umc/commonplant/CommonplantApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CommonplantApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
